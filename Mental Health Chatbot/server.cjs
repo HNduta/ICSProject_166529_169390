480c827f2c2b34173959d55dbdcf2ed50c8508d8
HNduta <hope.wanjohi@strathmore.edu>
2025-06-23 16:19:40 +0300

    adding a chat history in the chat page

--- Mental Health Chatbot/server.cjs
+++ Mental Health Chatbot/server.cjs
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/chatbotDB', {
 });
 
 // Load Conversation model
-const Conversation = require('./models/Conversation');
+const Conversation = require('Mental Health Chatbot\models\conversation.js');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
